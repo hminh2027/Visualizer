8533ab9fd382547fd905ec6e9e1490a92fafa40e
Minh <dackhers@gmail.com>
2022-02-27 13:57:59 +0700

    update size slider

--- src/components/footer/sliders/SizeSlider.js
+++ src/components/footer/sliders/SizeSlider.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { generateUniqueArray } from '../../../helpers/util'
+import { clearTimeouts, generateUniqueArray, resetColumns, resetStore } from '../../../helpers/util'
 import { store } from '../../../store/store'
 
 import styles from './SizeSlider.module.css'
@@ -11,6 +11,9 @@ const SizeSlider = () => {
     const changeSizeHandler = e => {
         dispatch({ type: 'SET_LENGTH', payload: e.target.value })
         dispatch({ type: 'SET_ARRAY', payload: generateUniqueArray(e.target.value) })
+        clearTimeouts(context.state.timers, dispatch)
+        resetColumns()
+        resetStore(dispatch)
     }
 
     return (
